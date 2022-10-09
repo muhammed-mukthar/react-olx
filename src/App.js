dc561223614bedf6903711b46e0d9a060884b329
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-09 17:38:46 +0530

    design done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+import Signup from './Pages/Signup'
+/**
+ * ?  =====Import Components=====
+ */
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Router>
+       <Route exact path='/'>
+      <Home />
+      </Route>
+      <Route exact path='/signup'>
+      <Signup />
+      </Route>
       
+      </Router>
     </div>
   );
 }
